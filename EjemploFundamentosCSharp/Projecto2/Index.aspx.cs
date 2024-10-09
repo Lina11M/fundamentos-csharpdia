a52eec22acf4c1bd305ec42a0ee2fc7f759a1e57
Lina Lopez <linamlopezm05@gmail.com>
2024-10-09 16:34:04 -0500

    Se crea la clase car y el formulario

---
+++ EjemploFundamentosCSharp/Projecto2/Index.aspx.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.UI;
+using System.Web.UI.WebControls;
+
+namespace Projecto2
+{
+    public partial class Index : System.Web.UI.Page
+    {
+        protected void Page_Load(object sender, EventArgs e)
+        {
+            //Invocar
+            mostrarMensaje();
+        }
+        //Metodo para mostrar el mensaje por el navegador 
+        public void mostrarMensaje()
+        {
+            //Se crea un objeto de tipo car
+            Car car = new Car();
+            string msj = car.stop();
+            LblMensaje.Text = msj;
+        }
+    }
+}
